4bdfc20b0b3732748445f0eaefcd4d09026f4fd7
Mriyam Tamuli <mbtamuli@gmail.com>
2019-11-11 23:47:28 +0530

    Updates

---
+++ portfolio/precache-manifest.6218ba27efdb891d0e81d9550ed1be70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15b19ec4cdfa9e301b08865cdab5f37",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af6fedabe1e926d6f53b",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d8026fc763ca2dce1af0",
+    "url": "/portfolio/static/js/2.126e4470.chunk.js"
+  },
+  {
+    "revision": "af6fedabe1e926d6f53b",
+    "url": "/portfolio/static/js/main.52833874.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  }
+]);
